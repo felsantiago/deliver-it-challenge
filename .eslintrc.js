47644d71f433f16a7d0ab7fd25974378761c625f
Felipe Santiago <fepuss@gmail.com>
2021-01-25 03:12:21 -0300

    refactor: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,19 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
-    jest: true,
+    jest: true
   },
   extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 12
   },
   rules: {
     'class-methods-use-this': 'off',
     'no-underscore-dangle': 'off',
     'max-classes-per-file': 'off',
     'global-require': 'off',
-    'import/no-dynamic-require': 'off',
-  },
-};
+    'import/no-dynamic-require': 'off'
+  }
+}
